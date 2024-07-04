d67f9e81fa47d31ced02c23b5df677084ca36914
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-04 13:35:20 +0100

    main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,6 @@ from typing import List
 import logging
 import os
 import mysql.connector
-import mysql
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -50,12 +49,34 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     database_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     database_name = os.getenv('PERSONAL_DATA_DB_NAME')
 
-    return mysql.connector.connect(
+    connection = mysql.connector.connect(
         user=username,
         password=password,
         host=database_host,
         database=database_name
     )
+    return connection
+
+
+def main():
+    """Logs the information about user records in a table."""
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
 
 def filter_datum(
@@ -97,3 +118,7 @@ class RedactingFormatter(logging.Formatter):
         original_message = super().format(record)
         return filter_datum(self.fields,
                             self.REDACTION, original_message, self.SEPARATOR)
+
+
+if __name__ == "__main__":
+    main()
