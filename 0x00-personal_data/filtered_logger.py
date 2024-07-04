24cfbaf9f8b64f641a1cb025628db7369ee6ea02
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-04 10:26:37 +0100

    regex obfuscation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
     """Function that returns the log message obfuscated"""
     pattern = '|'.join([f'(?<={field}=)[^{seperator}]+' for field in fields])
     return re.sub(pattern, redaction, message)
