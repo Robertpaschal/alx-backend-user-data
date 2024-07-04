b6dc5ce0cf238f449487677cdd34eb8aa76227dd
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-04 10:18:05 +0100

    regex obfuscation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,22 +6,15 @@ import re
 from typing import List
 
 
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
 def filter_datum(
         fields: List[str], redaction: str, message: str,
         seperator: str) -> str:
     """
     Function that returns the log message obfuscated
-
-    Arguments:
-        fields: a list of strings representing all fields to obfuscate
-        redaction: a string representing by what the field
-            will be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character
-            is separating all fields in the log line (message)
-
-    returns:
-        str: The log message with specified fields obfuscated.
     """
-    pattern = '|'.join([f'(?<={field}=)[^{seperator}]+' for field in fields])
-    return re.sub(pattern, redaction, message)
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, seperator), replace(redaction), message)
