c945ea04587f2e63f0aafb10aad04d9326326bba
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-08 12:37:04 +0100

    401 error endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def get_unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+    - an error message
+    """
+    abort(401)
