6b93ff8341356ad82586648aa29a380245f9730c
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-09 11:58:06 +0100

    Basci authentication class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" A module containing an authentication class
+that inherits from Auth"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """An authentication class that inherits from Auth"""
