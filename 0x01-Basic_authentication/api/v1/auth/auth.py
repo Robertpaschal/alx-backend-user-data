bc671a268835dbbc1f40d7ee1bff10a18997cc54
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-09 09:27:37 +0100

    request authorization

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -32,7 +32,6 @@ class Auth:
         if request is None:
             return None
         return request.headers.get('Authorization')
-        
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
