56e04a66bc6592f4e73d21ced0abe4a91e610e4d
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-10 12:44:05 +0100

    Session class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A session authentication module that inherits from Auth class"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """A class for session authentication that inherits from Auth"""
