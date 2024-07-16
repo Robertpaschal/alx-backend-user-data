90e233962bfed37d713afd9aa3dd83c8fb4be369
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-16 07:36:59 +0100

    Hashed password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A module containing password hashing for users"""
+import bcrypt
+ 
+def _hash_password(password: str) -> bytes:
+        """Hashes a password"""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
+
 from user import Base, User
 
 
