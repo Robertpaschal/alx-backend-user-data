1658a55989ae1dd968204e758d9db7821c440048
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-16 18:39:09 +0100

    Basic route in flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def welcome():
     """GET route that returns a JSON payload with a welcome message"""
