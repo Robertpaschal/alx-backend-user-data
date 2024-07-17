1aad9ec418ab2f8f00312716338dd28838dff603
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-17 12:47:29 +0100

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,5 +73,6 @@ class Auth:
         try:
             user = self._db.find_user_by(user_id=user_id)
             self._db.update_user(user.id, session_id=None)
+            return None
         except NoResultFound:
             pass
