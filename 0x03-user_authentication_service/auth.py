02f857048e1e6a957ad3b58c33fdc161710b64b2
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-17 12:52:11 +0100

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,8 +72,4 @@ class Auth:
         """Updates the corresponding user session ID to None"""
         if user_id is None:
             return None
-        try:
-            user = self._db.find_user_by(user_id=user_id)
-            self._db.update_user(user.id, session_id=None)
-        except NoResultFound:
-            return None
+        self._db.update_user(user_id, session_id=None)
