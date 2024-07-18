20cf1bc3e4a94b6ff784617b6415c5294c70edf7
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-18 05:45:13 +0100

    app test file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,9 +42,8 @@ class Auth:
         """Validates a user login using details"""
         try:
             user = self._db.find_user_by(email=email)
-            if user is not None:
-                return bcrypt.checkpw(
-                    password.encode('utf-8'), user.hashed_password,)
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password,)
         except NoResultFound:
             return False
 
