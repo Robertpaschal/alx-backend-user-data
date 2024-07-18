aa4d5ebef8423dd92aeb00a0654d4a2fb47fee09
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-18 03:54:27 +0100

    app test file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,14 +96,15 @@ def update_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    if not email or not reset_token or not new_password:
-        abort(403)
-
+    is_password_changed = False
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
+        is_password_changed = True
     except ValueError:
+        is_password_changed = False
+    if not is_password_changed:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
